fb3d39893bad0f7c05811d4ab18cdd0fe8c8925d
Siwoo Kim <skim327@myseneca.ca>
2018-08-25 14:40:53 +0900

    first commit

---
+++ app/src/main/java/android/siwoo/com/androidlabtemp/layout/LabFive.java
@@ -0,0 +1,14 @@
+package android.siwoo.com.androidlabtemp.layout;
+
+import android.siwoo.com.androidlabtemp.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LabFive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_five);
+    }
+}
