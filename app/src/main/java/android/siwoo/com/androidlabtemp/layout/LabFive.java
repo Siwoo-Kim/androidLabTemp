5e4b8e76cafc3ac99cfcd60a29f70b2dcdcb40f3
Siwoo Kim <skim327@myseneca.ca>
2018-08-26 14:59:11 +0900

    first commit

--- app/src/main/java/android/siwoo/com/androidlabtemp/layout/LabFive.java
+++ app/src/main/java/android/siwoo/com/androidlabtemp/layout/LabFive.java
@@ -1,13 +1,10 @@
 package android.siwoo.com.androidlabtemp.layout;
 
 import android.siwoo.com.androidlabtemp.R;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TabHost;
 
 public class LabFive extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
